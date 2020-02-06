d116a118298345b89967d19286ed2c1cc48fe687
Serge Bishyr <seriybg@gmail.com>
2020-02-06 10:07:56 +0200

    initial commit

---
+++ src/main/java/com/lviv/javaclub/springcloudstreaming/SpringCloudStreamingApplication.java
@@ -0,0 +1,13 @@
+package com.lviv.javaclub.springcloudstreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudStreamingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lviv/javaclub/springcloudstreaming/SpringCloudStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lviv.javaclub.springcloudstreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
